44e95c2df54ea9fbe81fd63751ccf634eadf9b18
Tee Kay <71384506+TejasKumar3005@users.noreply.github.com>
2023-11-01 15:55:25 +0530

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Svar',
+      title: 'App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: "BuxtonSketch",
